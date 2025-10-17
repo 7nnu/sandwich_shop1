5559217548d6a97d7606d609dd1ecfda9ac36467
7nnu <tmhooper09@gmail.com>
2025-10-17 01:27:14 +0100

    added multiple orderItemDisplay

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,22 @@ class App extends StatelessWidget {
       title: 'Sandwich Shop App',
       home: Scaffold(
         appBar: AppBar(title: const Text('Sandwich Counter')),
-        body: const Center(
-          child: OrderItemDisplay(5, 'Footlong')
-        ),
+        body: Center(
+            child: Container(
+          alignment: Alignment.center,
+          color: Colors.blue,
+          width: 1920.0,
+          height: 400.0,
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              OrderItemDisplay(3, 'BLT'),
+              OrderItemDisplay(5, 'Club'),
+              OrderItemDisplay(2, 'Veggie'),
+            ],
+          ),
+        )),
       ),
     );
   }
@@ -25,12 +38,15 @@ class OrderItemDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('$quantity $itemType sandwich(es): ${'🥪'* quantity}');
+    return Text( 
+      '$quantity $itemType sandwich(es): ${'🥪' * quantity}',
+      style: const TextStyle(fontSize: 35, color: Colors.green, fontWeight: FontWeight.bold),
+    );
+      
+      
   }
 }
-
 
 void main() {
   runApp(const App());
 }
-
