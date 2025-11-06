804cb84925d281ba5471b008dbf9e6180882e002
7nnu <tmhooper09@gmail.com>
2025-11-06 19:23:26 +0000

    Restructure folder into views, view_models and repositories, Add app_styles.dart for managing styles, Update main.dart to use app_styles.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sandwich_shop/views/app_styles.dart';
 
 void main() {
   runApp(const App());
@@ -46,7 +47,7 @@ class _OrderScreenState extends State<OrderScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sandwich Counter'),
+        title: const Text('Sandwich Counter', style: heading1),
       ),
       body: Center(
         child: Column(
@@ -61,11 +62,11 @@ class _OrderScreenState extends State<OrderScreen> {
               children: [
                 ElevatedButton(
                   onPressed: _increaseQuantity,
-                  child: const Text('Add'),
+                  child: const Text('Add', style: normalText),
                 ),
                 ElevatedButton(
                   onPressed: _decreaseQuantity,
-                  child: const Text('Remove'),
+                  child: const Text('Remove', style: normalText),
                 ),
               ],
             ),
@@ -84,6 +85,6 @@ class OrderItemDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
+    return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}', style:normalText);
   }
 }
